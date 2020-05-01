62f7cf56e08a47dafc81319b2287e80802659160
error <er97ror@gmail.com>
2020-05-01 18:45:22 +0430

    create house

---
+++ Assets/SampleScenes/Scripts/Shooter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public Rigidbody ball; 
+    public int power;
+
+    public int ballCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1") && ballCount != 0)
+        {
+            ballCount--;
+            Debug.Log("ay baba");
+            Rigidbody instance = Instantiate(ball, transform.position, transform.rotation) as Rigidbody;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+            instance.AddForce(fwd * power);
+        }
+    }
+}
